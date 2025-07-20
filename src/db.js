4b38a85390de4f771547f6cbeb043cb12e9695d8
krutheesh <krutheeshsiripuram@gmail.com>
2025-07-20 10:33:46 +0530

    back to previous file name and code

--- src/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
-// console.log("mmmmmm : ",process.env.MONGODB_URI)
-const connectDB = async () => {
-  await mongoose.connect(process.env.MONGODB_URI);
-};
-
-module.exports = connectDB;
