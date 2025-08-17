20720cc517d572fbdb5fe41b867cb97e832071b2
krutheesh <krutheeshsiripuram@gmail.com>
2025-08-17 17:11:23 +0530

    set sameSite and secure in cookieOptions to work in production

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,8 @@ const { upsertStreamUser } = require("../config/stream");
 const cookieOptions = {
   expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
   httpOnly: true,
+  sameSite: "None",
+  secure: true
 };
 //signup
 
